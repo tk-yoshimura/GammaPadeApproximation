1f4f07b94dd9c492ebc546000766935ad48a595f
tk-yoshimura <yoshimura8192@gmail.com>
2022-11-02 22:50:58 +0900

    + asm

---
+++ GammaFunctionFP64/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GammaFunctionFP64")]
+[assembly: AssemblyDescription("Gamma function double precision")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GammaFunctionFP64")]
+[assembly: AssemblyCopyright("Copyright © T.Yoshimura 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2E14BEF3-77F5-422A-A078-13CD70F6A174")]
+
+[assembly: AssemblyVersion("1.0.0.*")]
